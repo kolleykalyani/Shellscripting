efa51af2fd67907de6e5b9b31e180bb8436b969f
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 15:28:22 +0530

    adding script

---
+++ 08_Multiple_variable_all_arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+######################################################
+# Author: Kalyani									 #
+# Description: first script                          #
+# Date: 26-03-2026									 #
+######################################################
+# git add . && git commit -m "adding script" && git push
+#!/bin/bash
+
+#!/bin/bash
+
+echo "Files to list:"
+ls "$*"
