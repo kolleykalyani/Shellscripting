3787c7cd42a2a68d32f71776e4de97d8718f88f4
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 12:34:02 +0530

    adding code

---
+++ 02_variable_in_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+######################################################
+# Author: Kalyani									 #
+# Description: first script                          #
+# Date: 26-03-2026									 #
+######################################################
+echo "Hello world"
+echo
+echo "Hello world"
