8d650d546cbc2d5ed52fc151e88f20e8f485a196
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 13:03:10 +0530

    adding script

--- 04_Multiple_variable_in_runtime copy.sh
+++ 04_Multiple_variable_in_runtime copy.sh
@@ -4,8 +4,9 @@
 # Description: first script                          #
 # Date: 26-03-2026									 #
 ######################################################
+# git add . && git commit -m "adding script" && git push
 NAME=$1 
 NAME1=$2
 echo "Hello good morning" 
 echo "my name is $NAME"
-echo "$NAME1" is venkatesh wife
+echo "my second name is $NAME1"
