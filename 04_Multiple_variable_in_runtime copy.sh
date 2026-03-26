29d559141b8a1991b0c75c779edb6bf7b66f09cb
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 12:50:41 +0530

    multiple variables

---
+++ 04_Multiple_variable_in_runtime copy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+######################################################
+# Author: Kalyani									 #
+# Description: first script                          #
+# Date: 26-03-2026									 #
+######################################################
+NAME=$1 
+NAME1=$2
+echo "Hello good morning" 
+echo "my name is $NAME"
+echo "$NAME" is $NAME1 wife"
