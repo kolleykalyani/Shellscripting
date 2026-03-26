a00da6a1c7ce85a3fdbe64b78903913c3d714f02
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 15:03:16 +0530

    adding script

--- 07_Multiple_variable_with_lastexecution.sh
+++ 07_Multiple_variable_with_lastexecution.sh
@@ -5,9 +5,6 @@
 # Date: 26-03-2026									 #
 ######################################################
 # git add . && git commit -m "adding script" && git push
-NAME=$1 
-NAME1=$2
-echo "Number of arguments: $?"
-echo "Hello good morning" 
-echo "my name is $NAME"
-echo "my second name is $NAME1"
+mkdir test_folder
+
+echo $?
