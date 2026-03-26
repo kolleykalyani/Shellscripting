10e207a22384b3a355e4e9298c5bce1ef0f13bd5
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-26 13:07:17 +0530

    adding script

---
+++ 04_Multiple_variable_with_read.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+######################################################
+# Author: Kalyani									 #
+# Description: first script                          #
+# Date: 26-03-2026									 #
+######################################################
+# git add . && git commit -m "adding script" && git push
+read -p "Enter your name: " NAME
+read -p "Enter your second name: " NAME1
+echo "Hello good morning" 
+echo "my name is $NAME"
+echo "my second name is $NAME1"
