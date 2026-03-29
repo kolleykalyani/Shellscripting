59769b841524c29968967933dc4c37925bb0fe9b
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 15:36:38 +0530

    adding script

--- 16_subnet.sh
+++ 16_subnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT=$(aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId: .VolumeId, Device: .Attachments[].Device}')
+OUTPUT=$(aws ec2 describe-volumes | jq '.Volumes[] | {.VolumeId, Device: .Attachments[].Device}')
 for i in $OUTPUT
 do
     echo "Name of the volume and device: $i"
