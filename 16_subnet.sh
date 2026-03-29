bc4768451e5d3885df1c5c2d14298c78d0298858
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 15:46:01 +0530

    adding script

--- 16_subnet.sh
+++ 16_subnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT=$(aws ec2 describe-volumes | jq -r '.Volumes[] | {VolumeId: .VolumeId, Device: .Attachments[].Device}')
+OUTPUT=$(aws ec2 describe-volumes | jq -r '.Volumes[] | "\(.VolumeId) \(.Attachments[].Device)"')
 
 echo "$OUTPUT" | while read vol device
 do
