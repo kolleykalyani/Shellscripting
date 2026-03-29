b4bfa12f0ad9bb9e14aeb8e21ff7a7303f827c6f
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 17:21:33 +0530

    adding script

--- 16_subnet.sh
+++ 16_subnet.sh
@@ -2,9 +2,10 @@
 for snapshot in `aws ec2 describe-snapshots --owner-ids self | jq ".Snapshots[].SnapshotId" -r `
 do
  echo "we are going to delete $snapshot"
- aws ec2 delete-snapshot --snapshot-id $snapshot
+ 
  while [ $(aws ec2 describe-snapshots --snapshot-ids $snapshot | jq -r '.Snapshots[].State') != "deleted" ]
- do
+ do  
+    aws ec2 delete-snapshot --snapshot-id $snapshot
      sleep 2
      echo "waiting for snapshot to delete..."
  done    
