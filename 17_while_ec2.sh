a4476faf96c47613f05acec4d65d18313c77b8a7
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 15:59:55 +0530

    adding script

--- 17_while_ec2.sh
+++ 17_while_ec2.sh
@@ -5,6 +5,6 @@ do
     
     aws ec2 create-snapshot \
         --volume-id "$vol" \
-        --description "Snapshot of $vol"
+        --description "Snapshot of $vol" | jq -r '.SnapshotId'
 
 done
