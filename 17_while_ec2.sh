2496e80d4404de035761cc4ca3409fbe7c103629
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 17:07:14 +0530

    adding script

--- 17_while_ec2.sh
+++ 17_while_ec2.sh
@@ -2,7 +2,13 @@
 for volume in `aws ec2 describe-volumes | jq -r '.Volumes[].VolumeId'`
 do
     echo "Creating snapshot for $volume"
-    # aws ec2 create-snapshot \
-    #     --volume-id "$volume" \
-    #     --description "Snapshot of $volume" | jq -r '.SnapshotId'
+    SNAPSHOT_ID=$(aws ec2 create-snapshot --volume-id $volume --description "Snapshot of $volume" | jq -r '.SnapshotId')
+    echo "snapshot created: $SNAPSHOT_ID"
+    echo "waiting for snapshot to complete..."
+    while [ $(aws ec2 describe-snapshots --snapshot-ids $SNAPSHOT_ID | jq -r '.Snapshots[].State') != "completed" ]
+    do  
+        sleep 2
+        echo "waiting for snapshot to complete..."
+    done
+    echo "snapshot completed: $SNAPSHOT_ID"
 done
