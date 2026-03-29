24f94ff772885ee663894d0ebca99e8d4108a823
Kalyani <tellamekalakalyani1996@gmail.com>
2026-03-29 17:19:47 +0530

    adding script

--- 16_subnet.sh
+++ 16_subnet.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-OUTPUT=$(aws ec2 describe-volumes | jq -r '.Volumes[] | "\(.VolumeId) \(.Attachments[].Device)"')
-
-for OUTPUT in $OUTPUT
+for snapshot in `aws ec2 describe-snapshots --owner-ids self | jq ".Snapshots[].SnapshotId" -r `
 do
-    echo "$OUTPUT"
+ echo "we are going to delete $snapshot"
+ aws ec2 delete-snapshot --snapshot-id $snapshot
+ while [ $(aws ec2 describe-snapshots --snapshot-ids $snapshot | jq -r '.Snapshots[].State') != "deleted" ]
+ do
+     sleep 2
+     echo "waiting for snapshot to delete..."
+ done    
+ echo "snapshot deleted: $snapshot" 
 done
